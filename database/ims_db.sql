02ea48e45efe1676c0b1e62897a12202694a1590
chrisneetcode <rchrisorallo@gmail.com>
2025-07-12 23:13:48 +0800

    Updated to latest database.sql file

--- database/ims_db.sql
+++ database/ims_db.sql
@@ -1,67 +1,59 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 12, 2025 at 09:16 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: ims_db
+Target Host: localhost
+Target Database: ims_db
+Date: 7/12/2025 11:13:21 PM
+*/
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `ims_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_user`
---
-
-CREATE TABLE `tbl_user` (
-  `id` int(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `userlevel` varchar(255) NOT NULL
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for tbl_stocks
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_stocks`;
+CREATE TABLE `tbl_stocks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `supplier` varchar(255) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `unit_of_measure` varchar(255) NOT NULL,
+  `quantity` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `fund_cluster` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tbl_user`
---
+-- ----------------------------
+-- Table structure for tbl_supply
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_supply`;
+CREATE TABLE `tbl_supply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `supplier_name` varchar(255) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `quantity` varchar(255) NOT NULL,
+  `fund_cluster` varchar(255) NOT NULL,
+  `contact_person` int(11) NOT NULL,
+  `mobile_number` int(11) NOT NULL,
+  `date_added` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `tbl_user` (`id`, `username`, `password`, `userlevel`) VALUES
-(1, 'admin', 'admin', 'Admin');
+-- ----------------------------
+-- Table structure for tbl_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_user`;
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `userlevel` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tbl_user`
---
-ALTER TABLE `tbl_user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tbl_user`
---
-ALTER TABLE `tbl_user`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `tbl_user` VALUES ('1', 'admin', '$2y$10$qp8/XGUs9X5uC..EWTI5vurdZ1kU/GIQgW/p.dB8UWI3FNCMyG2bu', 'Admin');
